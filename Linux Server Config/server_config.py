f9e8b9b51b4f784c1aeab9e2a9b55c02142ccbac
Abdellah Oulahyane <aitbaha48@gmail.com>
2021-11-06 21:20:06 +0300

    Add files via upload

--- Linux Server Config/server_config.py
+++ Linux Server Config/server_config.py
@@ -1,6 +1,6 @@
 # /user/bin/env pythin3
 ##################################################################################
-#                           Author: Абдуллах Улахияне                            #
+#                           Author: Abdellah Oulahyane                           #
 #   File Name: /home/user/Desktop/project/Linux Server Config/server_config.py   #
 #                    Creation Date: October 24, 2021 03:34 PM                    #
 #                    Last Updated: October 24, 2021 03:34 PM                     #
@@ -23,36 +23,36 @@ HOSTS_PATH = "/etc/hosts"
 HOST_NAME = sys.argv[1]
 SEREVER_ADMIN = "admin@webserver2.com"
 SERVER_ALIAS = HOST_NAME if HOST_NAME.startswith("www.") else "www."+HOST_NAME
-DOCUMENT_ROOT = "mkdir /var/www/"+HOST_NAME+"/public_html"
-VIRTUAL_HOST = """<VirtualHost *:80>\n"\
-                           ServerAdmin   %s\n 
-                           ServerName    %s\n
-                           ServerAlias   %s\n 
-                           DocumentRoot  %s\n 
-                           ErrorLog ${APACHE_LOG_DIR}/error.log\n
-                           CustomLog ${APACHE_LOG_DIR}/access.log combined\n
-                    </VirtualHost> 
+DOCUMENT_ROOT = "/var/www/"+HOST_NAME+"/public_html"
+VIRTUAL_HOST = """<VirtualHost *:80>
+                     ServerAdmin   %s 
+                     ServerName    %s
+                     ServerAlias   %s 
+                     DocumentRoot  %s 
+                     ErrorLog ${APACHE_LOG_DIR}/error.log
+	             CustomLog ${APACHE_LOG_DIR}/access.log combined
+                  </VirtualHost> 
                 """ % (SEREVER_ADMIN, HOST_NAME, SERVER_ALIAS, DOCUMENT_ROOT)
 APACHE_CONFIG_PATH = "/etc/apache2/sites-available"
-print("Step 1")
+print("Step 1",end=" ")
 os.system("echo 127.0.0.1\t\t"+HOST_NAME+" >> "+HOSTS_PATH)
-print("Step 1 Done")
-print("Step 2 ")
+print(" Done")
+print("Step 2",end=" ")
 if os.path.isfile(APACHE_CONFIG_PATH+"/"+HOST_NAME+".conf"):
     os.system("rm "+APACHE_CONFIG_PATH+"/"+HOST_NAME+".conf")
 os.system("touch "+APACHE_CONFIG_PATH+"/"+HOST_NAME+".conf")
-print("Step 2 Done")
-print("Step 3")
-os.system("echo  "+VIRTUAL_HOST+" >> " +
+print(" Done")
+print("Step 3",end=" ")
+os.system("echo  '"+VIRTUAL_HOST+"' >> " +
           APACHE_CONFIG_PATH+"/"+HOST_NAME+".conf")
-print("Step 3 Done")
-print("Step 4")
+print(" Done")
+print("Step 4",end=" ")
 if not os.path.isdir("/var/www/"+HOST_NAME):
     os.system("mkdir /var/www/"+HOST_NAME)
 if not os.path.isdir("/var/www/"+HOST_NAME+"/public_html"):
     os.system("mkdir /var/www/"+HOST_NAME+"/public_html")
-print("Step 5 Done")
-print("Step 6")
+print(" Done")
+print("Step 5",end=" ")
 os.system("a2ensite "+HOST_NAME+".conf")
 os.system("service apache2 restart")
-print("Step 6 Done")
+print(" Done")
