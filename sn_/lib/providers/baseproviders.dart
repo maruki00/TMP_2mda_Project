73021f96150fcfd326d3cfa4604be21cd0813792
Abdellah Oulahyane <aitbaha48@gmail.com>
2021-11-20 23:45:22 +0300

    Delete sn_/lib directory

--- sn_/lib/providers/baseproviders.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get_it/get_it.dart';
-import 'package:sn/providers/feedsprovider.dart';
-import 'package:sn/providers/homeprovider.dart';
-
-GetIt backend = GetIt.instance();
-setup() {
-  backend.registerLazySingleton(() => FeedsProvider());
-  backend.registerLazySingleton(() => HomeProvider());
-}
--- sn_/lib/providers/homeprovider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class HomeProvider extends ChangeNotifier {
-  Widget? child;
-  changechild() {
-    notifyListeners();
-  }
-}
