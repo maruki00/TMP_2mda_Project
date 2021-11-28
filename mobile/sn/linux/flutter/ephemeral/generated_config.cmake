48d30350de569f55b52bac9b1d3f6ba8baac1c08
Abdellah Oulahyane <aitbaha48@gmail.com>
2021-11-29 01:35:30 +0300

    Add files via upload

---
+++ mobile/sn/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,15 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/user/development/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/user/Desktop/sn" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/user/development/flutter"
+  "PROJECT_DIR=/home/user/Desktop/sn"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/user/Desktop/sn/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/user/Desktop/sn/lib/main.dart"
+)
